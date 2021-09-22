05e03aa1eb94c54ef9ebc58de7837f6fc9e0a320
Ehtisham Ul Haq <46608858+ahtidotpk@users.noreply.github.com>
2021-09-22 18:07:40 +0500

    Add files via upload

--- js/brain.js
+++ js/brain.js
@@ -12,203 +12,265 @@ const distributer = document.querySelector(".completed");
 //buttons
 const inputBtn = document.querySelector(".btn-input");
 const moreBtn = document.querySelector(".more");
+const tickBtn = document.querySelector(".tick");
 
 //texts
 const todoText = document.querySelector(".todo__text");
 const items = document.querySelector(".items");
 
-//variables
-const baseHolder = { dataHolder: [] };
-let datanos = 0,
+class App {
+  //variables
+
+  baseHolder = { dataHolder: [] };
+  datanos = 0;
   currentItems = 0;
+  #edit;
+  #selectedNode;
 
-// deafaults
-all.style.color = "hsl(220, 98%, 61%)";
-//functions
-const defaultColors = (color1, color2, color3) => {
-  all.style.color = color1;
-  active.style.color = color2;
-  distributer.style.color = color3;
-};
-const HtmlCleaner = () => (todoContainer.innerHTML = "");
-// HtmlCleaner();
+  constructor() {
+    all.style.color = "hsl(220, 98%, 61%)";
+    inputBtn.addEventListener("click", this.storeData.bind(this));
+    todoContainer.addEventListener("click", this.navigation.bind(this));
+    todoInfoContainer.addEventListener("click", this.navigationBtn.bind(this));
+    this.getData();
+  }
 
-const todoInput = (status) => {
-  let html;
-  status.forEach((val, i) => {
-    const idValue = val?.id;
-    if (idValue != undefined) {
-      const text = val.text;
-      const status = val.status[0];
-      const decorationText =
-        status == `Completed`
-          ? `style="text-decoration: line-through;"`
-          : `style="text-decoration: none;"`;
-      const currentStatus = status == "Completed" ? `checked` : ``;
-      const decorationBg =
-        status == `Completed`
-          ? `style = "background-color: rgb(33, 35, 54);"`
-          : `background-color: hsl(234, 11%, 52%)`;
+  //functions
 
-      html = `
+  getData() {
+    this.baseHolder = JSON.parse(localStorage.getItem("todoHistory"));
+
+    this.todoInput([...this.baseHolder.dataHolder]);
+  }
+
+  setData() {
+    localStorage.setItem("todoHistory", JSON.stringify(this.baseHolder));
+  }
+
+  storeData(e) {
+    e.preventDefault();
+    const inputText = input.value;
+    this.baseHolder.dataHolder[this.datanos] = {
+      id: [],
+      status: [],
+      text: [],
+    };
+
+    if (inputText != "") {
+      this.baseHolder.dataHolder[this.datanos].id.push(this.datanos);
+      this.baseHolder.dataHolder[this.datanos].text.push(inputText);
+      this.baseHolder.dataHolder[this.datanos].status.push("Active");
+      this.currentItems++;
+      this.datanos++;
+      this.HtmlCleaner();
+      this.inIt();
+    }
+  }
+
+  todoInput = (status) => {
+    let html;
+    status.forEach((val, i) => {
+      const idValue = val?.id;
+      if (idValue != undefined) {
+        const text = val.text;
+        const status = val.status[0];
+        const decorationText =
+          status == `Completed`
+            ? `style="text-decoration: line-through;"`
+            : `style="text-decoration: none;"`;
+        const currentStatus = status == "Completed" ? `checked` : ``;
+        const decorationBg =
+          status == `Completed`
+            ? `style = "background-color: rgb(33, 35, 54);"`
+            : `background-color: hsl(234, 11%, 52%)`;
+
+        html = `
      <div class="todo" draggable="true" ${decorationBg}>
           <input type="checkbox" class="checkbox" ${currentStatus}  data-set="${i}">
           <span class="todo__text todo-${i}" data-set="${i}" ${decorationText}>${text}</span>
           <img src="./images/more .svg" " class="more" />
-           <span class="tick" >✓</span>
+           <span class="tick" data-set="${i}">✓</span>
           <img src="./images/icon-cross.svg"  class="cross" data-set="${i}"/>
      </div>
     `;
-      todoContainer.insertAdjacentHTML(`beforeend`, html);
-      items.textContent = currentItems;
-    }
-  });
+        todoContainer.insertAdjacentHTML(`beforeend`, html);
+        items.textContent = this.currentItems;
+      }
+    });
 
-  input.value = "";
-};
-
-todoEditer = (target, display) => {
-  target.style.display = display;
-};
-
-const currentStatus = (status, value) => {
-  baseHolder.dataHolder[value].status.splice(0, 1);
-  baseHolder.dataHolder[value].status.push(status);
-  HtmlCleaner();
-  todoInput([...baseHolder.dataHolder]);
-};
-
-inputBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  const inputText = input.value;
-  baseHolder.dataHolder[datanos] = {
-    id: [],
-    status: [],
-    text: [],
+    input.value = "";
   };
 
-  if (inputText != "") {
-    baseHolder.dataHolder[datanos].id.push(datanos);
-    baseHolder.dataHolder[datanos].text.push(inputText);
-    baseHolder.dataHolder[datanos].status.push("Active");
-    currentItems++;
-    datanos++;
-    HtmlCleaner();
-    todoInput([...baseHolder.dataHolder]);
-  }
-});
-
-todoContainer.addEventListener("click", function (e) {
-  //Dealing with cross
-  if (e.target.classList.contains("cross")) {
-    HtmlCleaner();
+  removeTodo(e) {
+    this.HtmlCleaner();
     const targetNumber = Number(e.target.dataset.set);
-    delete baseHolder.dataHolder[targetNumber];
-    currentItems--;
-    items.textContent = currentItems;
-    todoInput([...baseHolder.dataHolder]);
+    delete this.baseHolder.dataHolder[targetNumber];
+    this.currentItems--;
+    items.textContent = this.currentItems;
+    this.inIt();
   }
 
-  //dealing with three dots TODO:
-  if (e.target.classList.contains("more")) {
-    const input = document.createElement("input");
+  editTodo(e) {
     const node = e.target.parentNode.children;
-    const setValue = node[1].dataset.set;
+    this.#selectedNode = node[1].dataset.set;
+    this.#edit = node;
 
     //input structure
+    const input = document.createElement("input");
     input.type = "text";
     input.className = "input";
-    input.value = node[1].textContent;
+    input.value = this.#edit[1].textContent;
 
     //textNode structure
-    node[1].replaceWith(input);
-    todoEditer(node[0], "none");
-    todoEditer(node[2], "none");
-    todoEditer(node[3], "block");
+    this.#edit[1].replaceWith(input);
+    this.todoEditer(this.#edit[0], "none");
+    this.todoEditer(this.#edit[2], "none");
+    this.todoEditer(this.#edit[3], "block");
 
-    node[3].addEventListener("click", () => {
-      baseHolder.dataHolder[setValue].text.splice(0, 1);
-      baseHolder.dataHolder[setValue].text.push(input.value);
-      input.replaceWith(node[1]);
-      todoEditer(node[0], "block");
-      todoEditer(node[2], "block");
-      todoEditer(node[3], "none");
-      HtmlCleaner();
-      todoInput([...baseHolder.dataHolder]);
+    this.#edit[3]?.addEventListener("click", () => {
+      this.baseHolder.dataHolder[this.#selectedNode].text.splice(0, 1);
+      this.baseHolder.dataHolder[this.#selectedNode].text.push(input.value);
+      input.replaceWith(this.#edit[1]);
+      this.todoEditer(this.#edit[0], "block");
+      this.todoEditer(this.#edit[2], "block");
+      this.todoEditer(this.#edit[3], "none");
+      this.HtmlCleaner();
+      this.inIt();
     });
   }
 
-  //Dealing check Button
-  if (e.target.classList.contains("checkbox")) {
-    let completedWorkValue;
-    completedWorkValue = e.target.dataset.set;
+  checkboxTodo(e) {
+    const completedWorkValue = e.target.dataset.set;
     if (e.target.checked) {
-      currentStatus("Completed", completedWorkValue);
+      this.currentStatus("Completed", completedWorkValue);
     }
     if (!e.target.checked) {
-      currentStatus("Active", completedWorkValue);
+      this.currentStatus("Active", completedWorkValue);
     }
   }
-});
 
-todoInfoContainer.addEventListener("click", (e) => {
-  //Toggle All
-  if (e.target.classList.contains("all")) {
-    HtmlCleaner();
-    todoInput([...baseHolder.dataHolder]);
-    defaultColors(
+  checkAll(e) {
+    this.HtmlCleaner();
+    this.inIt();
+    this.defaultColors(
       "hsl(220, 98%, 61%)",
       "hsl(234, 11%, 52%)",
       "hsl(234, 11%, 52%)"
     );
   }
 
-  //Toggle Active
-  if (e.target.classList.contains("active")) {
-    const todoActive = [...baseHolder.dataHolder]?.filter(
+  checkActives(e) {
+    const todoActive = [...this.baseHolder.dataHolder]?.filter(
       (val) => val?.status[0] === "Active"
     );
-    HtmlCleaner();
-    todoInput(todoActive);
-    defaultColors(
+    this.HtmlCleaner();
+    this.todoInput(todoActive);
+    this.defaultColors(
       "hsl(234, 11%, 52%)",
       "hsl(220, 98%, 61%)",
       "hsl(234, 11%, 52%)"
     );
   }
 
-  //Toggle Completed
-  if (e.target.classList.contains("completed")) {
-    const todoCompleted = [...baseHolder.dataHolder]?.filter(
+  checkCompleted(e) {
+    const todoCompleted = [...this.baseHolder.dataHolder]?.filter(
       (val) => val?.status[0] === "Completed"
     );
-    HtmlCleaner();
-    todoInput(todoCompleted != undefined ? todoCompleted : []);
-    defaultColors(
+    this.HtmlCleaner();
+    this.todoInput(todoCompleted != undefined ? todoCompleted : []);
+    this.defaultColors(
       "hsl(234, 11%, 52%)",
       "hsl(234, 11%, 52%)",
       "hsl(220, 98%, 61%)"
     );
   }
 
-  //Clear Completed
-  if (e.target.classList.contains("clear-completed")) {
+  clearCompletedTodo(e) {
     let currentItemsValue = 0;
-    [...baseHolder.dataHolder]
+    [...this.baseHolder.dataHolder]
       ?.filter((val) => val?.status[0] === "Completed")
-      .map((val, i) => val.id[0])
+      .map((val) => val.id[0])
       .forEach((val) => {
-        delete baseHolder.dataHolder[val];
+        delete this.baseHolder.dataHolder[val];
         currentItemsValue++;
       });
-    currentItems -= currentItemsValue;
-    items.textContent = currentItems;
-    HtmlCleaner();
-    todoInput([...baseHolder.dataHolder]);
-    defaultColors(
+    this.currentItems -= currentItemsValue;
+    items.textContent = this.currentItems;
+    this.HtmlCleaner();
+    this.inIt();
+    this.defaultColors(
       "hsl(220, 98%, 61%)",
       "hsl(234, 11%, 52%)",
       "hsl(234, 11%, 52%)"
     );
   }
-});
+
+  ///////////////////////////////////
+  //HELPER FUNCTIONS
+
+  inIt() {
+    this.todoInput([...this.baseHolder.dataHolder]);
+    this.setData();
+  }
+
+  navigation(e) {
+    //Dealing with cross
+    if (e.target.classList.contains("cross")) {
+      this.removeTodo(e);
+    }
+
+    //dealing with three dots
+    if (e.target.classList.contains("more")) {
+      this.editTodo(e);
+    }
+
+    //Dealing check Button
+    if (e.target.classList.contains("checkbox")) {
+      this.checkboxTodo(e);
+    }
+  }
+
+  navigationBtn(e) {
+    //Toggle All
+    if (e.target.classList.contains("all")) {
+      this.checkAll(e);
+    }
+
+    //Toggle Active
+    if (e.target.classList.contains("active")) {
+      this.checkActives(e);
+    }
+
+    //Toggle Completed
+    if (e.target.classList.contains("completed")) {
+      this.checkCompleted(e);
+    }
+
+    //Clear Completed
+    if (e.target.classList.contains("clear-completed")) {
+      this.clearCompletedTodo(e);
+    }
+  }
+
+  defaultColors = (color1, color2, color3) => {
+    all.style.color = color1;
+    active.style.color = color2;
+    distributer.style.color = color3;
+  };
+
+  // HtmlCleaner();
+  HtmlCleaner = () => (todoContainer.innerHTML = "");
+
+  todoEditer(target, display) {
+    target.style.display = display;
+  }
+
+  currentStatus(status, value) {
+    this.baseHolder.dataHolder[value].status.splice(0, 1);
+    this.baseHolder.dataHolder[value].status.push(status);
+    this.HtmlCleaner();
+    this.inIt();
+  }
+}
+
+const todoApp = new App();
